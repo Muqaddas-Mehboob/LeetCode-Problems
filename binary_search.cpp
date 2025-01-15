2f9b7ee763d042672b81a361fbe5eb5c81194385
Muqaddas Mehboob <164070095+Muqaddas-Mehboob@users.noreply.github.com>
2025-01-15 15:29:30 +0500

    Add files via upload

---
+++ binary_search.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        // Time Complexity = O(logn)
+        // Space Complexity = O(1)
+        
+        int n = nums.size();
+        int start = 0;
+        int end = n - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+};

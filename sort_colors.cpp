533bf2d9d48c8b19008bfbdff151431bafa69546
Muqaddas Mehboob <164070095+Muqaddas-Mehboob@users.noreply.github.com>
2025-01-18 07:20:14 -0800

    Update sort_colors.cpp

--- sort_colors.cpp
+++ sort_colors.cpp
@@ -4,7 +4,8 @@ public:
 
       // Time Complexity = O(n^2)
       // Space Complexity = O(1)
-      // Apprach: Bubble Sort (This can also be solved using any other sorting algortihms.)
+      // Approach: Bubble Sort (This can be solved using other sorting algorithms.)
+        
         for(int i = 0; i < nums.size() - 1; i++){
             for(int j = 0; j < nums.size() - i - 1; j++){
                 if(nums[j] > nums[j + 1]){
@@ -12,5 +13,12 @@ public:
                 }
             }
         }
+    }
+    void sortColors(vector<int>& nums) {
+
+        // Time Complexity = O(nlogn)
+        // Space Complexity = O(1)
+        
+        sort(nums.begin(), nums.end());
     }
 };

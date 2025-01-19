3c9249d600ea47350683ff09ea654a8071fd0295
Muqaddas Mehboob <164070095+Muqaddas-Mehboob@users.noreply.github.com>
2025-01-19 07:08:03 -0800

    Update sort_colors.cpp

--- sort_colors.cpp
+++ sort_colors.cpp
@@ -21,4 +21,62 @@ public:
         
         sort(nums.begin(), nums.end());
     }
+
+    void sortColors(vector<int>& nums) {
+
+        // Time Complexity = O(n)
+        // Space Complexity = O(1)
+        // Approach: Counting 0's, 1's and 2's
+        
+        int n = nums.size();
+        int countOfZero = 0;
+        int countOfOnes = 0;
+        int countOfTwos = 0;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                countOfZero++;
+            }
+            else if(nums[i] == 1){
+                countOfOnes++;
+            }
+            else{
+                countOfTwos++;
+            }
+        }
+
+        int index = 0;
+        for(int i = 0; i < countOfZero; i++){
+            nums[index++] = 0;
+        }
+        for(int i = 0; i < countOfOnes; i++){
+            nums[index++] = 1;
+        }
+        for(int i = 0; i < countOfTwos; i++){
+            nums[index++] = 2;
+        }
+    }
+
+    void sortColors(vector<int>& nums) {
+
+        // Time Complexity = O(n)
+        // Space Complexity = O(1)
+        // Approach: Dutch National Flag Algorithm (A one-pass algorithm with constant space complexity)
+        
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[mid++], nums[low++]);
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[high--], nums[mid]);
+            }
+        }
+    }
 };

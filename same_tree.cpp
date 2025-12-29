e9abce4797410d0adb2bcb4b42b4a120e8f0afb7
Muqaddas Mehboob <164070095+Muqaddas-Mehboob@users.noreply.github.com>
2025-12-30 02:19:50 +0500

    Add isSameTree function for binary tree comparison
    Implement a function to check if two binary trees are the same.

---
+++ same_tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+        // TIME COMPLEXITY = O(N)
+        // SPACE COMPLEXITY = O(1)
+        
+        if(p == nullptr && q == nullptr) return true;
+        if(p == nullptr || q == nullptr) return false;
+
+        int temp1 = p->val;
+        int temp2 = q->val;
+
+        if(temp1 == temp2){
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+
+        return false;
+    }
+};
